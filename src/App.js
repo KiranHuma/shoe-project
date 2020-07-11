369aefaecb6b257591e9c93a43cc856118251d8e
aimalhuma61 <66478525+aimalhuma61@users.noreply.github.com>
2020-07-11 11:42:08 +0500

    Add dashboard and files of panacloud's video together

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './Dashboard';
+import Routes from './Route.js';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Dashboard/>
+    {/* <Routes /> */}
+    </div>
+  );
+}
+
+export default App;
